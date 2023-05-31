a1007696dffb4c6f8ff4889f9bd61801422034ee
Jovan Milojevic <jovanm028@gmail.com>
2023-06-01 00:51:55 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <img src="/./Home.png" alt="pocetna" className="w-full" />
+    </main>
+  )
+}
---
+++ src/app/product/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Product() {
+  return (
+    <div>
+        <img src="/./product.png" alt="product" className="w-full" />
+    </div>
+  )
+}
